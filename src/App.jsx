ee4ce7b6334722e32810a8d2b8100fe68c975f67
Colin Matteson <seefm@proton.me>
2024-08-04 19:21:23 -0400

    audio tag now working

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import './App.css'
+import song from './118707__teqstudios__perfect-timing.mp3'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -11,7 +12,7 @@ function App() {
         <h4>I'm a regular guy in his mid 30's, committed to learning how to live again. I've decided to document my journey here in a variety of different mediums. Thank you for visiting it!</h4>
       </div>
       <audio controls>
-        <source src='../assets/118707__teqstudios__perfect.mp3' />
+        <source src={song} />
       </audio>
     </>
   )
