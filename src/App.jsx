445a1d9458bf3bb0a03d2acf8d77efd9ba668aa6
Colin Matteson <seefm@proton.me>
2024-08-04 20:14:14 -0400

    messing around with styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,14 @@
 import { useState } from 'react'
 import './App.css'
+import { Outlet, Route, Routes } from 'react-router-dom'
 import song from './118707__teqstudios__perfect-timing.mp3'
+import ApplicationViews from './views/ApplicationViews'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <h1>JourneyFM</h1>
-        <h4>I'm a regular guy in his mid 30's, committed to learning how to live again. I've decided to document my journey here in a variety of different mediums. Thank you for visiting it!</h4>
-      </div>
-      <audio controls>
-        <source src={song} />
-      </audio>
-    </>
+<ApplicationViews />
   )
 }
 
