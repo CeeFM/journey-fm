bfd4822810d4b3840fda896316642104ff387664
Colin Matteson <seefm@proton.me>
2024-08-09 21:45:58 -0400

    adds some text for pictures page

--- src/views/Pictures.jsx
+++ src/views/Pictures.jsx
@@ -9,6 +9,9 @@ function Pictures() {
                 <h1>Pictures</h1>
                 <p className='w-50'>Here's where you can find all of the pictures I've added so far</p>
             </div>
+            <div>
+                Keep in mind I never said I was any good at taking pictures, but nevertheless, I like to take them.
+            </div>
         </>
     )
 }
